e7e888f6b1156a89836a829837eb962bbaf7d1eb
zana-a <zana200@icloud.com>
2023-11-09 18:45:52 +0000

    chore: add database support

--- src/main/java/io/zana/openflix/OpenFlixApp.java
+++ src/main/java/io/zana/openflix/OpenFlixApp.java
@@ -1,8 +1,12 @@
 package io.zana.openflix;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class OpenFlixApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, OpenFlix!");
+        SpringApplication.run(OpenFlixApp.class, args);
     }
 }
